435df75c4c805a991f6048f478e70efca5418ae6
Kalu David <148062083+Kalu1155@users.noreply.github.com>
2025-08-27 14:15:41 +0100

    new

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,55 +1,60 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import logo from "../assets/image/House-Market-Logo-scaled.jpg";
-import sticky_logo from "../assets/image/House-Market-Logo-scaled.jpg";
 import { Link } from "react-router-dom";
 import { RiCloseLine, RiMenu3Line } from "react-icons/ri";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
-  const [showMenu, setShowmenu] = useState(true);
-  const [closeMenu, setClosemenu] = useState(true);
-
 
   return (
-    <>
-      <div id="site-wrapper">
-        <div id="site-header">
-          <div id="masthead" className="header-v4">
-            <div className="wrapper">
-              <div className="brand has-logo has-sticky-logo">
-                <h1 className="logo"><Link to="/"><img src={logo} alt="Konstruct" /></Link></h1>
-              </div>
-              <nav className="navigator">
-                <ul className="menu">
-                  <li>
-                    <Link to='/about' className="text-decoration-none">ABOUT US</Link>
-                  </li>
-                  <li><Link to='/mortgageconsulting' className="text-decoration-none">Mortgage consulting</Link></li>
-                  <li><Link to="/realestateconsulting" className="text-decoration-none">Real estate consulting</Link></li>
-                  <li><Link to="/partners" className="text-decoration-none">Partners</Link></li>
-                  <li><Link to="/contact" className="text-decoration-none">Contact</Link></li>
-                </ul>
-              </nav>
-            </div>
-              <nav id="site-navigator-mobile" class="navigator-mobile">
-                <Link to="#" class="navigator-toggle">
-                  <i class="fa fa-bars"></i>
+    <div id="site-wrapper">
+      <div id="site-header">
+        <div id="masthead" className="header-v4">
+          <div className="wrapper">
+            {/* Logo */}
+            <div className="brand has-logo has-sticky-logo">
+              <h1 className="logo">
+                <Link to="/">
+                  <img src={logo} alt="Konstruct" />
                 </Link>
-                <ul id="menu-main-menu-1" class="menu">
-                   <li>
-                    <Link to='/about' className="text-decoration-none">ABOUT US</Link>
-                  </li>
-                  <li><Link to='/mortgageconsulting' className="text-decoration-none">Mortgage consulting</Link></li>
-                  <li><Link to="/realestateconsulting" className="text-decoration-none">Real estate consulting</Link></li>
-                  <li><Link to="/partners" className="text-decoration-none">Partners</Link></li>
-                  <li><Link to="/contact" className="text-decoration-none">Contact</Link></li>
-                </ul>
-              </nav>
+              </h1>
+            </div>
+
+            {/* Desktop Nav */}
+            <nav className="navigator">
+              <ul className="menu">
+                <li><Link to="/about">ABOUT US</Link></li>
+                <li><Link to="/mortgageconsulting">Mortgage consulting</Link></li>
+                <li><Link to="/realestateconsulting">Real estate consulting</Link></li>
+                <li><Link to="/partners">Partners</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+              </ul>
+            </nav>
+
+            {/* Mobile Nav */}
+            <nav
+              id="site-navigator-mobile"
+              className={`navigator-mobile ${menuOpen ? "active" : ""}`}
+            >
+              <button
+                className="navigator-toggle"
+                onClick={() => setMenuOpen(!menuOpen)}
+              >
+                {menuOpen ? <RiCloseLine size={28} /> : <RiMenu3Line size={28} />}
+              </button>
+              <ul className="menu">
+                <li><Link to="/about" onClick={() => setMenuOpen(false)}>ABOUT US</Link></li>
+                <li><Link to="/mortgageconsulting" onClick={() => setMenuOpen(false)}>Mortgage consulting</Link></li>
+                <li><Link to="/realestateconsulting" onClick={() => setMenuOpen(false)}>Real estate consulting</Link></li>
+                <li><Link to="/partners" onClick={() => setMenuOpen(false)}>Partners</Link></li>
+                <li><Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link></li>
+              </ul>
+            </nav>
           </div>
         </div>
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
